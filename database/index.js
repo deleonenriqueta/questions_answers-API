feb087b1ca104e0a7e1d1e9067adc6da30277303
deleonenriqueta <enriqueta.correa@gmail.com>
2022-02-18 00:30:34 -0800

    updated insert to batches

--- database/index.js
+++ database/index.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
+const format = require('pg-format');
 const fastcsv = require('fast-csv');
-const pool = require('./pgdb');
-import { processQuestions, processAnswers, processPhotos} from ('./queries.js');
+const pool = require('./pgdbTest');
+const {processPhotos} = require('./queries.js');
 
 pool.connect((err) => {
   if (err) {
@@ -12,28 +13,40 @@ pool.connect((err) => {
   }
 });
 
+const multiInsert = (text, dataArr, counter) => {
+  pool.query(format(text + ' %L', dataArr), (err, res) => {
+    if (err) {
+      console.log('ERROR inserting row into DB: ', err);
+    } else {
+      console.log('Inserted up to: ', counter);
+    }
+  });
+}
+
 const readCSV = () => {
   var counter = 0;
-  var stream = fs.createReadStream('data/answers_photos.csv');
+  var dataArr = [];
+  var query = {};
+  var stream = fs.createReadStream('../data/answers_photos.csv');
   var csvStream = fastcsv
     .parse()
     .on('data', (data) => {
       csvStream.pause();
-      if(counter !== 0) {
-        var query = processPhotos(data);
-        pool.query(query.text, query.values, (err, res) => {
-          if (err) {
-            console.log('ERROR inserting row into DB: ', err);
-          } else {
-            console.log('ADDED: ', query.values[0]);
-          }
-        });
+      if(counter !== 0 ) {
+        query = processPhotos(data);
+        dataArr.push(query.values);
+        if (dataArr.length === 100 ) {
+          multiInsert(query.text, dataArr, counter);
+          dataArr = [];
+        }
       }
       counter ++;
       csvStream.resume();
     })
     .on('end', () => {
+      multiInsert(query.text, dataArr, counter);
       console.log('Job is done!');
+      return;
     })
     .on('error', (err) => {
       console.log(err);
