8a5b3c29784e57d8f6831d039988498dff83bea0
deleonenriqueta <enriqueta.correa@gmail.com>
2022-02-18 00:28:47 -0800

    updated query text

--- database/queries.js
+++ database/queries.js
@@ -1,15 +1,15 @@
 const processQuestions = (data) => {
-  let id            = Number(data[0]);
-  let product_id    = Number(data[1]);
-  let body          = data[2];
-  let asker_date    = data[3];
-  let asker_name    = data[4];
-  let asker_email   = data[5];
-  let reported      = Boolean(data[6]);
-  let helpful       = Number(data[7]);
+  let id              = Number(data[0]);
+  let product_id      = Number(data[1]);
+  let body            = data[2];
+  let date_written    = data[3];
+  let asker_name      = data[4];
+  let asker_email     = data[5];
+  let reported        = Boolean(data[6]);
+  let helpful         = Number(data[7]);
 
-  var text = 'INSERT INTO questions VALUES ($1, $2, $3, $4, $5, $6, $7, $8)';
-  var values = [id, product_id, body, asker_date, asker_name, asker_email, reported, helpful];
+  var text = 'INSERT INTO questions (id, product_id, body, date_written, asker_name, asker_email, reported, helpful) VALUES ';
+  var values = [id, product_id, body, date_written, asker_name, asker_email, reported, helpful];
 
   return {text, values};
 }
@@ -24,7 +24,7 @@ const processAnswers = (data) => {
   let reported        = Boolean(data[6]);
   let helpful         = Number(data[7]);
 
-  var text = 'INSERT INTO answers VALUES ($1, $2, $3, $4, $5, $6, $7, $8)';
+  var text = 'INSERT INTO answers (id, question_id, body, date_written, answerer_name, answerer_email, reported, helpful) VALUES ';
   var values = [id, question_id, body, date_written, answerer_name, answerer_email, reported, helpful];
 
   return {text, values};
@@ -35,7 +35,7 @@ const processPhotos = (data) => {
   let answer_id   = Number(data[1]);
   let url         = data[2];
 
-  var text = 'INSERT INTO answers_photos VALUES ($1, $2, $3)';
+  var text = 'INSERT INTO answers_photos (id, answer_id, url) VALUES ';
   var values = [id, answer_id, url];
 
   return {text, values};
