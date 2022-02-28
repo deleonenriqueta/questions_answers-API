8e4ce7bcae80899238f4751d2511f350ebdc62c1
deleonenriqueta <enriqueta.correa@gmail.com>
2022-02-27 18:16:13 -0800

    working on adding answers to questions

--- app.js
+++ app.js
@@ -8,22 +8,37 @@ const app = express();
 // });
 
 app.get('/qa/questions', (req, res) => {
+  var finalQuestions = [];
   dbQueries.pullAllQuestions(req.query.product_id, (err, result) => {
+    var questions = result.results;
     if (err) {
       res.send({err});
     } else {
-      for (var id of result.results) {
-        dbQueries.pullAllAnswers(id.question_id, (err, res) => {
-          if (err) {
-            res.send({err});
-          } else {
-            id.answers = res.answers;
-          }
-        });
+      let applyAnswers = async (questions) => {
       }
-      result = JSON.stringify(result);
-      res.send(result);
+
+
+      dbQueries.pullAllAnswers(questions, (err, result) => {
+        if (err) {
+          throw err;
+        } else {
+          res.send(result);
+        }
+      })
     }
+    //   for (var question of result.results) {
+    //   dbQueries.pullAllAnswers(question.question_id, (err, result) => {
+    //     if (err) {
+    //       throw err;
+    //     } else {
+    //       console.log(result.answers);
+    //       question.answers = result.answers;
+    //       promises.push(Promise(question));
+    //     }
+    //   });
+    // }
+    // result = JSON.stringify(result);
+    // res.send(result);
   })
 });
 
@@ -31,7 +46,6 @@ app.get('/qa/questions/:question_id/answers', (req, res) => {
   var questionId = req.params.question_id;
   dbQueries.pullAllAnswers(questionId, (err, result) => {
     if (err) {
-      console.log(err);
       res.send({err});
     } else {
       console.log(result);
