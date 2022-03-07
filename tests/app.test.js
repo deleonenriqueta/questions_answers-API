f0a650a5d4c1eedf0e53bf3fc19f38fbdc183c15
deleonenriqueta <enriqueta.correa@gmail.com>
2022-03-07 09:22:39 -0800

    updated tests

--- tests/app.test.js
+++ tests/app.test.js
@@ -10,40 +10,38 @@ const app = require('../app.js');
 //   })
 // })
 
-describe('GET /qa/questions', () => {
-  describe('a product id is provided', () => {
-    // // should pull the questions from the db
-    // // should respond with JSON object containing the list of questions
-    test('should respond with a 200 status code', async () => {
-      const response = await request(app).get('/qa/questions').send({
-        product_id: 12345
-      })
-      expect(response.statusCode).toBe(200);
-    });
-    test('should specify JSON in the content type header', async () => {
-      const response = await request(app).get('/qa/questions').send({
-        product_id: 12345,
-      })
-      expect(response.headers['content-type']).toEqual(expect.stringContaining('json'));
-    });
-    test('response should have product_id', async () => {
-      const response = await request(app).get('/qa/questions').send({
-        product_id: 12345
-      })
-      expect(response.body.results).toBeDefined();
-    });
-    xtest('valid product_id not provided', async () => {
-      const response = await request(app).get('/questions').send({
-        questionId: 12345
-      })
-      expect(response.statusCode).toBe(400);
-    });
-  })
+// describe('GET /qa/questions', () => {
+//   describe('a valid product_id was provided', () => {
+//     // // should pull the questions from the db
+//     // // should respond with JSON object containing the list of questions
+//     test('should respond with a 200 status code', async () => {
+//       const response = await request(app).get('/qa/questions').send({
+//         product_id: 1000010
+//       })
+//       expect(response.statusCode).toBe(200);
+//     });
+//     test('should specify JSON in the content type header', async () => {
+//       const response = await request(app).get('/qa/questions').send({
+//         product_id: 1000010
+//       })
+//       expect(response.headers['content-type']).toEqual(expect.stringContaining('json'));
+//     });
+//     test('response should have product_id', async () => {
+//       const response = await request(app).get('/qa/questions?product_id=1000010').send({
+//         product_id: 1000010
+//       })
+//       expect(response.body.product_id).toBeDefined();
+//     });
+//   })
 
-  describe('an invalid user id is provided', () => {
-    // should respond with a code of 400 to indicate user error
-  })
-})
+//   describe('a valid product_id was NOT provided', () => {
+//     // should respond with a code of 400 to indicate user error
+//     test('should respond with a code of 400', async () => {
+//       const response = await request(app).get('/qa/questions');
+//       expect(response.statusCode).toBe(400);
+//     })
+//   })
+// })
 
 // describe('GET /answers', () => {
 //   describe('a valid user id is provided', () => {
@@ -68,29 +66,29 @@ describe('GET /qa/questions', () => {
 //   })
 // })
 
-// describe('POST /questions', () => {
-//   describe('a question is provided', () => {
-//     // // should save the qustion to the db
-//     // // should respond with JSON object containing the user id
-//     test('should respond with a 200 status code', async () => {
-//       const response = await request(app).post('/questions').send({
-//         productId: 12345,
-//         body: 'some question',
-//         asker_name: 'someone',
-//         asker_email: 'first.last@gmail.com'
-//       })
-//       expect(response.statusCode).toBe(200);
-//     })
-//     test('should specify JSON in the content type header', async () => {
-//       const response = await request(app).post('/questions').send({
-//         productId: 12345,
-//         body: 'some question',
-//         asker_name: 'someone',
-//         asker_email: 'first.last@gmail.com'
-//       })
-//       expect(response.headers['content-type']).toEqual(expect.stringContaining('json'));
-//     })
-//   })
+describe('POST /questions', () => {
+  describe('a question is provided', () => {
+    // // should save the qustion to the db
+    // // should respond with JSON object containing the user id
+    test('should respond with a 200 status code', async () => {
+      const response = await request(app).post('/qa/questions').send({
+        productId: 12345,
+        body: 'some question',
+        asker_name: 'someone',
+        asker_email: 'first.last@gmail.com'
+      })
+      expect(response.statusCode).toBe(200);
+    })
+    test('should return status code 400 if request does not contain all required params', async () => {
+      const response = await request(app).post('/qa/questions').send({
+        body: 'some question',
+        asker_name: 'someone',
+        asker_email: 'first.last@gmail.com'
+      })
+      expect(response.statusCode).toBe(400);
+    })
+  })
+});
 
 //   describe('when question is missing', () => {
 //     // should respond with a code of 400 to indicate user error
