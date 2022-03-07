08d34c3f52190c1d39d185cc15767536d937ffca
deleonenriqueta <enriqueta.correa@gmail.com>
2022-03-07 09:21:38 -0800

    updated answers func

--- database/indexDB.js
+++ database/indexDB.js
@@ -1,5 +1,19 @@
 const pool = require('./ETL/pgdb.js');
 
+const paramsCheck = (params) => {
+  try {
+    const paramKeys = ['body', 'name', 'email', 'product_id'];
+    for (var key of paramKeys) {
+      if (!params.includes(key)) {
+        return false;
+      }
+    }
+    return true;
+  } catch (error) {
+
+  }
+}
+
 const pullAllQuestions = async (productId) => {
   try {
     const questions = await pool.query(`SELECT  question_id, question_body, question_date,
@@ -16,16 +30,70 @@ const pullAllQuestions = async (productId) => {
 const pullAllAnswers = async (question) => {
   try {
     const text = `SELECT  id, body, date, answerer_name, helpfulness
-                  FROM answers WHERE question_id = ($1) AND reported = 'false'`;
-    const values = [question.question_id];
-    const answers = await pool.query(text, values);
-    const transformedAnswers = await transformAnswers(answers.rows);
-    const finalAnswers = await pullAllPhotos(answers.rows);
-    question.answers = transformedAnswers;
-    return question;
+                  FROM answers WHERE question_id = ${question.question_id} AND reported = 'false'`;
+    const text2 = `SELECT id, answer_id, url
+                  FROM answers_photos WHERE question_id = ${question.question_id}`;
+    const answers = await pool.query(text);
+    const photos = await pool.query(text2);
+    if (answers.rows.length === 0) {
+      question.answers = {};
+      return question;
+    } else {
+      if (photos.rows.length === 0) {
+        question.answers = {};
+        for (var answer of answers.rows) {
+          answer.photos = [];
+          question.answers[answer.id] = answer;
+        }
+        return question;
+      } else {
+        const newAnswers = updateAnswers(answers.rows, photos.rows);
+        const newQuestion = updateQuestion(question, newAnswers);
+        return newQuestion;
+      }
+    }
+    // const transformedAnswers = await transformAnswers(answers.rows);
+    // const finalAnswers = await pullAllPhotos(answers.rows);
+    // question.answers = transformedAnswers;
+    // return question;
   } catch (error) {
     throw error;
   }
+}
+
+const updateAnswers = (answers, urls) => {
+  var urlsAndId;
+  var found;
+  var counter;
+
+  for (var url of urls) {
+    urlAnsId = url.answer_id;
+    found = false;
+    counter = 0;
+    while (!found) {
+      if (urlAnsId === answers[counter].id) {
+        if (answers[counter].photos) {
+          delete url.answer_id;
+          answers[counter].photos.push(url);
+        } else {
+          answers[counter].photos = [];
+          delete url.answer_id;
+          answers[counter].photos.push(url);
+        }
+        found = true;
+      }
+      counter ++;
+    }
+  }
+  return answers;
+}
+
+const updateQuestion = (question, answers) => {
+  question.answers = {};
+  for (var answer of answers) {
+    question.answers[answer.id] = answer;
+  }
+  return question;
 }
 
 const transformAnswers = async (answerObj) => {
@@ -82,5 +150,6 @@ const insertQuestion = async (params) => {
 module.exports = {
   pullAllQuestions,
   pullAllAnswers,
-  insertQuestion
+  insertQuestion,
+  paramsCheck
 }
